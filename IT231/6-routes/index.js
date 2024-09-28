69d25a939ea82943a082b841b4892649dcea47c8
Christian <Christianortiz039@gmail.com>
2024-09-28 00:03:01 -0500

    completed week7 and fix other things

--- IT231/6-routes/index.js
+++ IT231/6-routes/index.js
@@ -1,20 +1,37 @@
 "use strict"; 
+
+const routeResponseMap = {
+    '/info': '<h1>Info Page</h1>',
+    '/contact': '<h1>Contact Page</h1>',
+    '/about': '<h1>About Page.</h1>',
+    '/error': '<h1>Sorry the page isnt here.</h1>'
+}
+
 const port = 3000,
 
 http = require("http"),
 httpStatus = require("http-status-codes"), 
-app = http.createServer();
-
-app.on("request", (req, res) => {
-    res.writeHead(httpStatus.StatusCodes.OK,{
-
+app = http.createServer((req, res) => { 
+    res.writeHead(200,{
         "Content-Type": "text/html"
     });
-
-    let responseMessage = "<h1>This will show on the screen</h1>";
-
-    res.end(responseMessage);
+    if(routeResponseMap[req.url]){
+        res.end(routeResponseMap[req.url]);
+    }else{
+        res.end("<h1>Welcome!</h1>");
+    }
 });
+
+// app.on("request", (req, res) => {
+//     res.writeHead(httpStatus.StatusCodes.OK,{
+
+//         "Content-Type": "text/html"
+//     });
+
+//     let responseMessage = "<h1>This will show on the screen</h1>";
+
+//     res.end(responseMessage);
+// });
 
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
---
+++ IT231/7-confetti-cuisine-cuisine-node/index.js
@@ -0,0 +1,20 @@
+'use script'; 
+
+module.exports = {
+    html: {
+     "Content-type": 'text/html'
+    }, 
+   text: {
+     "Content-type": 'text/plain'
+   },
+   js:{
+    "Content-type": 'image/jpg'
+   },
+   png:{
+    "Content-type": 'image/png'
+   },
+   css:{
+    "Content-type": 'text/css'
+   },
+
+}
